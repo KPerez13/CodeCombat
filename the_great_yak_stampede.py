fcbd3829a0f1d2bfef9b1dfd9fc827c9fb0289ac
Kenny_wMBP <124447672+KPerez13@users.noreply.github.com>
2024-04-09 18:18:48 -0400

    desert_levels_04_09_2024

---
+++ the_great_yak_stampede.py
@@ -0,0 +1,12 @@
+# Save the village by blocking the path of the incoming sand yak herd.
+# Move closer to the red X.
+# Build a fence at the red X.
+
+hero.moveXY(71, 26)
+hero.moveXY(55, 17)
+hero.moveXY(10, 33)
+hero.moveXY(14, 54)
+hero.moveXY(38, 52)
+hero.buildXY("fence", 38, 50)
+
+
