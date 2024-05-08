d4170f68c1df598c8cde3d0e4f67a694a9ffe391
K.Perez_Work <124447672+KPerez13@users.noreply.github.com>
2024-05-08 04:59:25 -0400

    one_level_on_the_7th

---
+++ minesweeper.py
@@ -0,0 +1,19 @@
+# Lead the peasants and healer through the minefield.
+
+while True:
+    coin = hero.findNearestItem()
+    healingThreshold = hero.maxHealth / 2
+    # Check to see if you are critically injured.
+    if hero.health < healingThreshold:
+        # Move left 10m.
+        x = hero.pos.x
+        y = hero.pos.y
+        x -= 10
+        hero.moveXY(x, y)
+        # Ask for a heal.
+        hero.say("Can I get a heal?")
+        pass
+    # Else, move to the next coin.
+    elif coin:
+        hero.moveXY(coin.pos.x, coin.pos.y)
+
